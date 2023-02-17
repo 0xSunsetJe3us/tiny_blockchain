fe76e56cba7dcaa9f858e69ee38d4003446ce42e
SunsetJesus <729345318@qq.com>
2023-02-18 02:10:49 +0800

    [EDIT] fix bug, complete codes and support print the whole merker-tree for debug

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"tiny_blockchain/merkel_tree"
+)
 
+var testData []byte
+
+func initData() [][]byte {
+	var data [][]byte
+	for i := 0; i < 4; i++ {
+		str := fmt.Sprintf("test data %d", i)
+		bz, _ := json.Marshal(str)
+		if i == 3 {
+			testData = bz
+		}
+		data = append(data, bz)
+	}
+	return data
+}
+
+func main() {
+	data := initData()                                      // 1. init data
+	merkelTree, _ := merkel_tree.NewMerkelTree("md5", data) // 2. build a merkel tree
+	//fmt.Printf("%x", merkel_tree.GetMerkelRootHashValue())
+	merkelTree.PrintWholeTree()
 }
--- merkel_tree/merkel_tree.go
+++ merkel_tree/merkel_tree.go
@@ -1,11 +1,13 @@
 package merkel_tree
 
 import (
+	"container/list"
 	"crypto/md5"
 	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
 	"errors"
+	"fmt"
 	"hash"
 	"math"
 	"strings"
@@ -27,7 +29,7 @@ type node struct {
 	data      []byte
 }
 
-//@brief: 创建一个新的默克尔树
+// NewMerkelTree @brief: 创建一个新的默克尔树
 func NewMerkelTree(hashType string, data [][]byte) (*MerkelTree, error) {
 	var err error
 	merkelTree := &MerkelTree{}
@@ -54,14 +56,36 @@ func (m *MerkelTree) buildMerkelTreeRoot(data [][]byte) (*node, []*node, error) 
 
 // @brief: 构造默克尔树非根父节点, 递归
 func (m *MerkelTree) buildMerkelTreeNode(leaves []*node) (*node, error) {
-	mergeNodesCnt := int(math.Ceil(float64(len(leaves) / 2))) // 要合并的个数
-	// 两两hash成根节点, 如果只有一个，就增加一个节点, hash自己
+	var parentsNodes []*node
+	mergeNodesCnt := int(math.Ceil(float64(len(leaves) / 2))) // 要合并的个数, 向上舍入, 只会出现rightNode不够的情况
 	for i := 0; i < mergeNodesCnt; i++ {
-		leftNode := leaves[i]
-		rightNode := leaves[i+1]
+		leftNode := leaves[i*2]
+		rightNode := leftNode
+		if len(leaves) >= i*2+1 {
+			rightNode = leaves[i*2+1] // 两两hash成根节点, 如果只有一个，就增加一个节点, hash自己
+		}
+		// seems like single is useless...
+		// build a parentNode
+		n := &node{
+			parent:    nil,
+			left:      leftNode,
+			right:     rightNode,
+			leaf:      false,
+			single:    false,
+			data:      nil,
+			hashValue: m.callHashHandler(append(leftNode.hashValue, rightNode.hashValue...)),
+		} // combine left and right nodes' hash
+		leftNode.parent = n // let leaf points their parents.
+		rightNode.parent = n
+
+		parentsNodes = append(parentsNodes, n) // insert to parentNodes' list
 	}
 
-	return nil, nil
+	// recursive, processing parentsNodes when the number of nodes is 1, which is the-merkel-root
+	if len(parentsNodes) > 1 {
+		return m.buildMerkelTreeNode(parentsNodes)
+	}
+	return parentsNodes[0], nil
 }
 
 //@brief: 构造默克尔树叶节点, 将所有交易info构造成叶子
@@ -74,7 +98,7 @@ func (m *MerkelTree) buildMerkelTreeLeaves(data [][]byte) []*node {
 	return leaves
 }
 
-//@brief: 获取默克尔根
+// GetMerkelRootHashValue @brief: 获取默克尔根
 func (m *MerkelTree) GetMerkelRootHashValue() []byte {
 	return m.root.hashValue
 }
@@ -100,4 +124,39 @@ func (m *MerkelTree) callHashHandler(data []byte) []byte {
 	handler := m.hashHandler()
 	handler.Write(data)
 	return handler.Sum(nil)
+}
+
+// PrintWholeTree @brief: 打印整颗树的info, post-loop
+func (m *MerkelTree) PrintWholeTree() {
+	cnt := 0
+	nextCnt := 1
+	height := 1
+	// use bfs
+	queue := list.New()
+	queue.PushBack(m.root)
+	for queue.Len() != 0 {
+		e := queue.Front()
+		queue.Remove(e)
+		n, _ := e.Value.(*node)
+
+		cnt += 1
+		if cnt%nextCnt == 0 {
+			fmt.Printf("-- The-%d-level --\n", height)
+			nextCnt = int(math.Exp2(float64(height)))
+			height += 1
+		}
+
+		if n.left != nil || n.right != nil {
+			fmt.Printf("[Parents] data: %s, hash: %x, left: %x, right: %x\n", string(n.data), n.hashValue, n.left.hashValue, n.right.hashValue)
+		} else {
+			fmt.Printf("[Leaf] data: %s, hash: %x, left: %s, right: %s\n", string(n.data), n.hashValue, "null", "null")
+		}
+
+		if n.left != nil {
+			queue.PushBack(n.left)
+		}
+		if n.right != nil {
+			queue.PushBack(n.right)
+		}
+	}
 }
